71e347db29588d45768bc201f5b176e8038a077e
Jai Sharma <sharmajai@gmail.com>
2025-12-28 17:29:24 +0000

    Add Tensor trait with dimensionality working.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![feature(generic_const_exprs)]
+#![allow(incomplete_features)]
+
+use num_traits::Num;
+
+// enum Assert<const COND: bool> {}
+// trait IsTrue {}
+// impl IsTrue for Assert<true> {}
+
+trait Tensor<V: Num, const D: usize> {
+    const C: usize;
+
+    fn value(&self, index: usize) -> impl Tensor<V, { D - 1 }>
+    where
+        [(); D - 1]:;
 }
 
+// impl<V: Num, const C: usize, const D: usize> Tensor<V, C, D>
+//     for [Box<dyn Tensor<V, C, { D - 1 }>>; C]
+// {
+//     fn value(&self, index: usize) -> Tensor<i32, 2, 1> {
+//         // ...
+//     }
+// }
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn it_works() {}
 }
